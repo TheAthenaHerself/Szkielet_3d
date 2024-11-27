20ad0ab2d71c712098e4b5d9306f439ffb08d11b
Ryszard Markowski <ryszardjanuszmarkowski@gmail.com>
2024-11-27 11:03:26 +0100

    more cleanup

--- Szkielet_3d/Direct3d.cpp
+++ Szkielet_3d/Direct3d.cpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 #include <chrono>
 
+#include "Throws.h"
 #include "Direct3d.h"
 
 using Microsoft::WRL::ComPtr;
@@ -21,8 +22,6 @@ namespace {
 	UINT m_rtvDescriptorSize;
 
 	UINT_PTR timer;
-
-	HRESULT error_code;
 
 	UINT m_frameIndex = 0;
 	HANDLE m_fenceEvent;
@@ -56,19 +55,14 @@ namespace {
 		}
 #endif
 		ComPtr<IDXGIFactory7> factory;
-		error_code = CreateDXGIFactory2(0, IID_PPV_ARGS(&factory));
-		if (FAILED(error_code)) {
-			throw std::runtime_error("Fail at DXGIFactory2");
-		}
+		ThrowIfFailed(CreateDXGIFactory2(0, IID_PPV_ARGS(&factory)), "DXGIFactory2");
 
-		error_code = D3D12CreateDevice(
+		ThrowIfFailed(D3D12CreateDevice(
 			nullptr,
 			D3D_FEATURE_LEVEL_12_0,
 			IID_PPV_ARGS(&m_device)
-		);
-		if (FAILED(error_code)) {
-			throw std::runtime_error("Fail at Device");
-		}
+		), "Device");
+
 		D3D12_COMMAND_QUEUE_DESC queueDesc = {
 			.Type = D3D12_COMMAND_LIST_TYPE_DIRECT,
 			.Priority = D3D12_COMMAND_QUEUE_PRIORITY_HIGH,
@@ -76,10 +70,7 @@ namespace {
 			.NodeMask = 0,
 		};
 
-		error_code = m_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_commandQueue));
-		if (FAILED(error_code)) {
-			throw std::runtime_error("Fail at CommandQueue");
-		}
+		ThrowIfFailed(m_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_commandQueue)), "CommandQueue");
 
 		DXGI_SAMPLE_DESC sampleDesc = {
 			.Count = 1,
@@ -113,23 +104,16 @@ namespace {
 		};
 
 		ComPtr<IDXGISwapChain1> swapChain;
-		error_code = factory->CreateSwapChainForHwnd(
+		ThrowIfFailed(factory->CreateSwapChainForHwnd(
 			m_commandQueue.Get(),        // Swap chain needs the queue so that it can force a flush on it.
 			hwnd,
 			&swapChainDesc1,
 			&swapChainFullscreen,
 			nullptr,
 			&swapChain
-		);
+		), "SwapChainHwnd");
 
-		if (FAILED(error_code)) {
-			throw std::runtime_error("Fail at SwapChainHwnd");
-		}
-		//error_code = swapChain->QueryInterface(IID_PPV_ARGS(&m_swapChain));
-		error_code = swapChain.As(&m_swapChain);
-		if (FAILED(error_code)) {
-			throw std::runtime_error("Fail at swapChain3");
-		}
+		ThrowIfFailed(swapChain.As(&m_swapChain), "swapChain3");
 
 		D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {
 			.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV,
@@ -138,10 +122,7 @@ namespace {
 			.NodeMask = 0,
 		};
 
-		error_code = m_device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&m_rtvHeap));
-		if (FAILED(error_code)) {
-			throw std::runtime_error("Fail at DescriptorHeap");
-		}
+		ThrowIfFailed(m_device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&m_rtvHeap)), "DescriptorHeap");
 		m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 
 
@@ -150,69 +131,40 @@ namespace {
 		// Create a RTV for each frame.
 		for (UINT n = 0; n < FrameCount; n++)
 		{
-			error_code = m_swapChain->GetBuffer(n, IID_PPV_ARGS(&m_renderTargets[n]));
-			if (FAILED(error_code)) {
-				throw std::runtime_error("Fail at GetBuffer");
-			}
+			ThrowIfFailed(m_swapChain->GetBuffer(n, IID_PPV_ARGS(&m_renderTargets[n])), "GetBuffer");
 			m_device->CreateRenderTargetView(m_renderTargets[n].Get(), nullptr, rtvHandle);
 
 			m_rtvHandles[n] = rtvHandle;
 
 			rtvHandle.ptr += m_rtvDescriptorSize;
 		}
-		error_code = m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator));
-		if (FAILED(error_code)) {
-			throw std::runtime_error("Fail at CreateCommandAllocator");
-		}
+		ThrowIfFailed(m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator)), "CreateCommandAllocator");
 	}
 	
 	void LoadAssets() {
-		error_code = m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT,
+		ThrowIfFailed(m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT,
 			m_commandAllocator.Get(), nullptr, 
-			IID_PPV_ARGS(&m_commandList));
-		if (FAILED(error_code)) {
-			throw std::runtime_error("Fail at Create CommandList");
-		}
-		error_code = m_commandList->Close();
-		if (FAILED(error_code)) {
-			throw std::runtime_error("Fail at close command list");
-		}
-		error_code = m_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence));
-		if (FAILED(error_code)) {
-			throw std::runtime_error("Fail at create fence");
-		}
+			IID_PPV_ARGS(&m_commandList)), "Create CommandList");
+		ThrowIfFailed(m_commandList->Close(), "close command list");
+		ThrowIfFailed(m_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence)), "create fence");
 		m_fenceValue = 1;
 
 		// Create an event handle to use for frame synchronization.
 		m_fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
 		if (m_fenceEvent == nullptr)
 		{
-			error_code = HRESULT_FROM_WIN32(GetLastError());
-			if (FAILED(error_code)) {
-				throw std::runtime_error("Fail at create fence event");
-			}
+			ThrowIfFailed(HRESULT_FROM_WIN32(GetLastError()), "create fence event");
 		}
 
 		// Wait for the command list to execute; we are reusing the same command 
 		// list in our main loop but for now, we just want to wait for setup to 
 		// complete before continuing.
-		try {
-			WaitForPreviousFrame();
-		}
-		catch (std::runtime_error &er) {
-			throw er;
-		}
+		WaitForPreviousFrame();
 	}
 
 	void PopulateCommandList() {
-		error_code = m_commandAllocator->Reset();
-		if (FAILED(error_code)) {
-			throw std::runtime_error("Fail at reset command allocator");
-		}
-		error_code = m_commandList->Reset(m_commandAllocator.Get(), nullptr);
-		if (FAILED(error_code)) {
-			throw std::runtime_error("Fail at reset command list");
-		}
+		ThrowIfFailed(m_commandAllocator->Reset(), "reset command allocator");
+		ThrowIfFailed(m_commandList->Reset(m_commandAllocator.Get(), nullptr), "reset command list");
 
 		D3D12_RESOURCE_TRANSITION_BARRIER barrier = {
 			.pResource = m_renderTargets[m_frameIndex].Get(),
@@ -253,28 +205,19 @@ namespace {
 		};
 		m_commandList->ResourceBarrier(1, &pbarrier);
 
-		error_code = m_commandList->Close();
-		if (FAILED(error_code)) {
-			throw std::runtime_error("Fail at close command list");
-		}
+		ThrowIfFailed(m_commandList->Close(), "close command list");
 		m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
 	}
 
 	void WaitForPreviousFrame()
 	{
 		const UINT64 fence = m_fenceValue;
-		error_code = m_commandQueue->Signal(m_fence.Get(), fence);
-		if (FAILED(error_code)) {
-			throw std::runtime_error("Fail at signal command queue");
-		}
+		ThrowIfFailed(m_commandQueue->Signal(m_fence.Get(), fence), "signal command queue");
 		m_fenceValue++;
 
 		if (m_fence->GetCompletedValue() < fence)
 		{
-			error_code = m_fence->SetEventOnCompletion(fence, m_fenceEvent);
-			if (FAILED(error_code)) {
-				throw std::runtime_error("Fail at setEvent");
-			}
+			ThrowIfFailed(m_fence->SetEventOnCompletion(fence, m_fenceEvent), "setEvent");
 			WaitForSingleObject(m_fenceEvent, INFINITE);
 		}
 
@@ -283,13 +226,8 @@ namespace {
 }
 
 void OnInit(HWND hwnd) {
-	try {
-		LoadPipeline(hwnd);
-		LoadAssets();
-	}
-	catch (std::runtime_error& er) {
-		throw er;
-	}
+	LoadPipeline(hwnd);
+	LoadAssets();
 }
 
 void OnUpdate(){
@@ -302,27 +240,14 @@ void OnRender()
 	ID3D12CommandList* ppCommandLists[] = { m_commandList.Get() };
 	m_commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
 
-	error_code = m_swapChain->Present(1, 0);
-	if (FAILED(error_code)) {
-		throw std::runtime_error("Fail at Present");
-	}
+	ThrowIfFailed(m_swapChain->Present(1, 0), "Present");
 
-	try {
-		WaitForPreviousFrame();
-	}
-	catch (std::runtime_error& er) {
-		throw er;
-	}
+	WaitForPreviousFrame();
 }
 
 void OnDestroy() {
-	try {
-		WaitForPreviousFrame();
-		CloseHandle(m_fenceEvent);
-	}
-	catch (std::runtime_error& er) {
-		throw er;
-	}
+	WaitForPreviousFrame();
+	CloseHandle(m_fenceEvent);
 }
 
 void InitTimer(HWND hwnd) {
